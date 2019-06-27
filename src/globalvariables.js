aeb6597e8d9fbd0b42b6c7aa2d2ac2555a823eb6
Rasel Karim <rasel.romoni.xyz@gmail.com>
2019-06-27 12:00:12 +0600

    pusher credential was imported from env to gbvar

--- src/globalvariables.js
+++ src/globalvariables.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 const ADMIN_URL = process.env.VUE_APP_ADMIN_URL;
 const BASE_URL = process.env.VUE_APP_BASE_URL;
+
+const PUSHER_APP_ID = process.env.VUE_APP_PUSHER_APP_ID;
+const PUSHER_APP_KEY = process.env.VUE_APP_PUSHER_APP_KEY;
+const PUSHER_APP_SECRET = process.env.VUE_APP_PUSHER_APP_SECRET;
+const PUSHER_APP_CLUSTER = process.env.VUE_APP_PUSHER_APP_CLUSTER;
+
 export default {
     axios: axios,
     LS_TOKEN_KEY_NAME: 'token',
@@ -30,10 +36,10 @@ export default {
     SECRET_KEY: 'Kali Nagin Ke Jaisi Julfe Teri Kali Kali',
     SECRET_KEY_SAME_AS_PARTNER_PANEL: 'Dust in the wind.', /* TO_EDIT_THIS_PLACE, This has to be same as Partner panel gbvar SECRET_KEY*/
     PUSHER_CREDENTIALS: {
-        APP_ID:'743021',
-        APP_KEY:'8671a1da7b105f413bb1',
-        APP_SECRET:'21f6c57273a0c5cab00f',
-        APP_CLUSTER:'ap2'
+        APP_ID: PUSHER_APP_ID,
+        APP_KEY: PUSHER_APP_KEY,
+        APP_SECRET: PUSHER_APP_SECRET,
+        APP_CLUSTER: PUSHER_APP_CLUSTER
     }
 
 }

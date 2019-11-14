28edd265f1009a076cc5ca06fb3efd79676f8957
romoni.xyz <oronno@romoni.xyz>
2019-11-14 17:05:47 +0600

    datetime parsing carbon,promo edit(1)

--- src/views/Promos/PromoEdit.vue
+++ src/views/Promos/PromoEdit.vue
@@ -158,14 +158,14 @@
       },
 
       changeDateFormat(){
-        let d = this.expires_at;
+        let d = this.promo.expires_at;
         let month = '' + (d.getMonth() + 1);
         let day = '' + d.getDate();
         let year = d.getFullYear();
         if (month.length < 2) month = '0' + month;
         if (day.length < 2) day = '0' + day;
 
-        this.expires_at =  [year, month, day].join('-');
+        this.promo.expires_at =  [year, month, day].join('-');
       },
       onSubmit(e) {
         this.changeDateFormat();

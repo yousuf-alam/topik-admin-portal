75f6043f62a4db89768e4bd9cb90b18776a13eba
ali079 <ali@nagorik.tech>
2024-05-12 20:02:07 +0600

    product export

--- src/views/ProductRequest/ProductRequests.vue
+++ src/views/ProductRequest/ProductRequests.vue
@@ -12,10 +12,59 @@
         </router-link>
       </div>
     </div>
+    <b-row>
+      <modal name="modal-order_export" height="auto" :adaptive="true">
+        <div class="m-3 p-3">
+          <b-row class="p-2 border-bottom">
+            <h4>Export Product Request as .xlsx</h4><br><br>
+          </b-row>
+          <b-row class="p-2">
+           <b-col>
+            <div class="form-group">
+              <label class="font-weight-bold">Select Date Range:</label>
+              <b-row>
+                <b-col>
+                  <label>From:</label>
+                  <datepicker calendar-class="vdp-datepicker" v-model="date_from" format="yyyy-MM-dd"></datepicker>
+                </b-col>
+                <b-col>
+                  <label>To:</label>
+                  <datepicker calendar-class="vdp-datepicker" v-model="date_to" format="yyyy-MM-dd"></datepicker>
+                </b-col>
+              </b-row>
+            </div>
 
+             <div class="form-group">
+               <label class="font-weight-bold">Select Date Type:</label>
+               <select class="form-control" v-model="date_type">
+                 <option selected value="requisition_date">Requisition Date</option>
+                 <option value="created_at">Created at</option>
+               </select>
+             </div>
+             <div class="form-group">
+               <label class="font-weight-bold">Select Status:</label>
+               <select class="form-control" v-model="status">
+                 <option selected value="all">All</option>
+                 <option value="requested">Requested</option>
+                 <option value="approved">Approved</option>
+                 <option value="sent">Sent</option>
+                 <option value="received">Received</option>
+                 <option value="rejected">Rejected</option>
+               </select>
+             </div>
+             <button @click="closeModal" class="btn btn-danger float-right m-1">Cancel</button>
+             <button @click="ExportProductRequest" class="btn btn-romoni-secondary float-right m-1">Export Product Request Report</button>
+             <b-spinner variant="danger" label="Spinning" v-if="exporting"></b-spinner>
+           </b-col>
+          </b-row>
+
+        </div>
+      </modal>
+    </b-row>
     <b-row>
       <b-col>
         <b-card>
+          <button  @click="modalExport" class="btn btn-success mb-2"><i class="fa fa-file-excel-o"></i> Export as .xlsx </button>
           <v-client-table :data="productRequests" :columns="columns" :options="options">
             <template slot="action" slot-scope="props">
               <div class="d-flex gap-2">
@@ -38,17 +87,34 @@
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
 const ADMIN_URL = process.env.VUE_APP_ADMIN_URL;
-
+import Vue from 'vue';
+import paginate from 'vuejs-paginate';
+import _ from 'lodash';
+import Datepicker from 'vuejs-datepicker';
+import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+import store from "../../store/store";
+Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker);
 
 export default {
   name: 'ProductRequests',
+  components: {
+        Datepicker
+      },
   data() {
     return {
       productRequests: [],
       columns: [
         'id', 'partner_name', 'status', 'requisition_date', 'acquisition_period', 'total_price', 'send_date', 'approved_by', 'created_at', 'action'
       ],
+      date_from: '',
+      date_to: '',
+      date_type: 'requisition_date',
+      status: 'all',
+      exporting: false,
+
       options: {
         pagination: { nav: 'fixed' },
         filterByColumn: true,
@@ -71,9 +137,53 @@ export default {
           console.error("Error while fetching product requests", error);
         });
     },
-    // handleDelete(id) {
-    //   // Similar confirmation and deletion logic
-    // },
+
+    modalExport() {
+            this.$modal.show('modal-order_export');
+          },
+          closeModal(){
+            this.$modal.hide('modal-order_export')
+          },
+          ExportProductRequest(){
+            this.exporting = true;
+            console.log(this.date_type);
+            console.log(this.date_from);
+            console.log(this.date_to);
+            console.log(this.status);
+            axios({
+              method: 'post',
+              url: `${ADMIN_URL}/product-request/export`,
+              responseType: 'blob',
+              data: {
+
+                date_type: this.date_type,
+                status: this.status,
+                date_from: this.date_from,
+                date_to: this.date_to
+
+              }
+
+
+            })
+              .then(response => {
+
+                this.exporting = false;
+
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', 'product_request_report_' + moment(this.date_from).format('YYYY-MM-DD') + '~' + moment(this.date_to).format('YYYY-MM-DD') +'.xlsx');
+                document.body.appendChild(link);
+                link.click();
+                this.$swal('Report Exported Successfully', '', 'success');
+                this.closeModal();
+              })
+              .catch(e => {
+                console.log("error occurs",e);
+              });
+
+
+          },
   },
 
 }

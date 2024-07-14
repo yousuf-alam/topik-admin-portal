973c1b7a4fd2c7b9051c493fbfc94f1c1f31fb8d
yousuf-alam <yousufalam1998@gmail.com>
2024-07-14 17:22:19 +0600

    completed status shwon to all

--- src/views/Orders/Orders.vue
+++ src/views/Orders/Orders.vue
@@ -596,11 +596,11 @@
             const shipping_phone = this.getInputValue("shipping_phone");
             const shipping_address = this.getInputValue("shipping_address");
 
-            let setStatusCom = 0;
-            if(this.getUserPermission("admin")){
-              setStatusCom = 1;
-            }
-            const statusCompleted = setStatusCom;
+            // let setStatusCom = 0;
+            // if(this.getUserPermission("admin")){
+            //   setStatusCom = 1;
+            // }
+            const statusCompleted = 0;
             //console.log( "Shipping_Phone ", shipping_phone, typeof( this.dateRange.created_at));
             let from = '';
             let to = '';
